c97ade541a8b260e2a6a249e0a279c06911fd41f
haliljon <juraboevhaliljon@gmail.com>
2023-11-24 02:28:20 +0500

    Refactor links

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000', 'https://timely-cactus-44d00e.netlify.app/'
+        origins 'https://timely-cactus-44d00e.netlify.app/'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
     end
 end
