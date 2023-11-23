9f41655eeb96eda98837873ef45ee9828db2843c
haliljon <juraboevhaliljon@gmail.com>
2023-11-24 02:35:55 +0500

    Refactor links

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://timely-cactus-44d00e.netlify.app'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
+      origins do |origin, _env|
+        # Check if the origin is allowed (e.g., a Netlify subdomain)
+        # You can implement your own logic here
+        allowed_origins = ['https://timely-cactus-44d00e.netlify.app', 'http://localhost:3001']
+        allowed_origins.include?(origin) || origin.start_with?('https://subdomain-of-netlify.netlify.app')
+      end
+      resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
     end
-end
+  end
+  
