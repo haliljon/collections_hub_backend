a113019c93f5a41a3aa020f7d6ad030cafa7126e
haliljon <juraboevhaliljon@gmail.com>
2023-11-25 13:38:54 +0500

    refactor registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,19 +1,20 @@
 class RegistrationsController < ApplicationController
     def create
-        user=User.create!(
+        begin
+          user = User.create!(
             email: params["user"]["email"],
             username: params["user"]["username"],
             password: params["user"]["password"],
             password_confirmation: params["user"]["password_confirmation"]
-        )
-        if user
-            session[:user_id]=user.id
-            render json: {
-                status: :created,
-                user: user
-            }
+          )
+        rescue ActiveRecord::RecordInvalid => e
+          render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
         else
-            render json: { status: 500 }
+          session[:user_id] = user.id
+          render json: {
+            status: :created,
+            user: user
+          }
         end
-    end
+      end
 end
