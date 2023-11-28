d39312e8eaf207cd95f20b92314da1121149e2c8
haliljon <juraboevhaliljon@gmail.com>
2023-11-28 10:57:32 +0500

    refactor to work on netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://timely-cactus-44d00e.netlify.app', 'http://localhost:3000'
+        origins 'https://collections-hub.netlify.app', 'http://localhost:3000'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head], credentials: true
     end
 end
